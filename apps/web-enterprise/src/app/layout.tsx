ea9a8d7692bf2dd61208c32e53abf9392aa5d3bd
masamasaowl <aryancheersjee@gmail.com>
2026-01-15 22:41:47 +0530

    create utilities

--- apps/web-enterprise/src/app/layout.tsx
+++ apps/web-enterprise/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from './components/AuthProvider';
+import { ToastProvider } from '@/contexts/ToastContext';
+import { SWRConfig } from 'swr';
 
 // We configure our font
 const inter = Inter({ subsets: ['latin'] });
@@ -21,10 +23,26 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
 
-        {/* To Authorize the user at the start of the app we wrap them all pages */}
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        {/* Enable SWR and pass the possible used values */}
+        <SWRConfig
+          value={{
+            revalidateOnFocus: true,
+            revalidateOnReconnect: true,
+            shouldRetryOnError: true,
+            errorRetryCount: 3,
+            dedupingInterval: 5000,
+          }}
+        >
+            {/* The Toaster is available throughout the app */}
+            <ToastProvider>
+              {/* To Authorize the user at the start of the app we wrap them all pages */}
+              <AuthProvider>
+                {children}
+              </AuthProvider>
+            </ToastProvider>
+        </SWRConfig>
+        
+        
       </body>
     </html>
   );
